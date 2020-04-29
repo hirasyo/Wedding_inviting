d85203ac2d2d8d647c0ea9efcd38efa4bf177e9e
shoichiro.hirata@v-vanguard.co.jp <shoichiro.hirata@v-vanguard.co.jp>
2020-04-29 20:05:26 +0900

    add create_members_migration file

---
+++ database/migrations/2020_04_29_105927_create_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('氏名');
+            $table->boolean('出席');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('members');
+    }
+}
